8a66539d293a605b96db62f85dde5a0cbb626c32
Tanay Shahi <tanayshahiofficial@gmail.com>
2026-07-10 08:15:30 +0530

    added output image for procedure. auth file secured. errors resolved

--- Week 1/DSA/Exercise 2/product.java
+++ Week 1/DSA/Exercise 2/product.java
@@ -4,7 +4,7 @@ class Product {
     String productName;
     String category;
 
-    public Product(int productId, String productName, String category) {
+    public void setProduct(int productId, String productName, String category) {
         this.productId = productId;
         this.productName = productName;
         this.category = category;
--- Week 3/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
+++ Week 3/spring-learn/spring-learn/src/main/java/com/cognizant/spring_learn/controller/AuthenticationController.java
@@ -1,18 +1,19 @@
-package com.cognizant.springlearn.controller;
+package com.cognizant.spring_learn.controller;
 
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Base64;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @RestController
 public class AuthenticationController {
